78bdaa7fb2a5b7125e5981a57d15f00d3a6abd92
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-04-23 19:57:41 +0200

    Update CallbackGraveyards.php

--- API - source code/app/Http/Controllers/CallbackGraveyards.php
+++ API - source code/app/Http/Controllers/CallbackGraveyards.php
@@ -10,17 +10,8 @@
      * @access public
      * @return callable
      */
-
-    /**
-     * [Failure]: Invalid parse option
-     *
-     * @access public
-     * @return callable.
-     */
-    public function CallbackInvalidParseOption() {
-      return [
-        'error'   => (bool) true,
-        'message' => (string) 'Invalid parse option',
-        ];
+    public function transformNoGraveyard() {
+      
     }
+    
   }
