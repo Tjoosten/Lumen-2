5bcce8fbb97ee42b3443b32f30b15071a2b4d924
Tjoosten <Tjoosten3@gmail.com>
2015-04-24 02:59:08 +0200

    Update

--- API-source-code/app/Console/Kernel.php
+++ API-source-code/app/Console/Kernel.php
@@ -1,18 +1,20 @@
-<?php namespace App\Console;
+<?php
 
-use Illuminate\Console\Scheduling\Schedule;
-use Laravel\Lumen\Console\Kernel as ConsoleKernel;
+  namespace App\Console;
 
-class Kernel extends ConsoleKernel
-{
+  use Illuminate\Console\Scheduling\Schedule;
+  use Laravel\Lumen\Console\Kernel as ConsoleKernel;
+
+  class Kernel extends ConsoleKernel {
 
     /**
      * The Artisan commands provided by your application.
      *
      * @var array
-     */
+      */
     protected $commands = [
       'App\Console\Commands\Rowcount',
+      // 'App\Console\Commands\Faker',
     ];
 
     /**
@@ -21,8 +23,7 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
-        //
+    protected function schedule(Schedule $schedule) {
+      // Scheduled jobs.
     }
-}
+  }
