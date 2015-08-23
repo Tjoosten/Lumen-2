8955329b49b461c53b76370d30d1007ad235b169
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-08-24 01:23:38 +0200

    Update .issue-guidelines.js

--- .issue-guidelines.js
+++ .issue-guidelines.js
@@ -21,35 +21,4 @@ var assert = require('assert')
 
 module.exports = {
 
-    'issues': {
-
-        'before': function (issue) {
-            var plus   = {}
-            var labels = issue.labels.map(function (label) { return label.name });
-
-            if (~labels.indexOf('popular')) return
-
-            issue.comments.forEach(function (comment) {
-                if (/\+1/.test(comment.body)) plus[comment.user.login] = true
-            })
-
-            if (Object.keys(plus).length > 5) {
-                issue.tag('popular')
-                issue.comment('Tagging this issue as popular, please stop commenting on this issue with +1. thanks!')
-            }
-        },
-
-        'should include a jsfiddle/jsbin illustrating the problem if tagged with js but not a feature': function (issue) {
-            var labels = issue.labels.map(function (label) { return label.name });
-            if (~labels.indexOf('js') && !~labels.indexOf('feature')) assert.ok(/(jsfiddle|jsbin)/.test(issue.body))
-        },
-
-        'after': function (issue) {
-            if (issue.reporter.stats.failures) {
-                issue.reportFailures(issue.close.bind(issue))
-            }
-        }
-
-    }
-
 }
